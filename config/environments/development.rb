fe137c1f65fe2d4695f08cde29c877ae7b61aeb2
CaioFML <caio.felipemotta@hotmail.com>
2020-11-03 19:41:39 -0300

    4 - Configurando o envio de email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,15 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'localhost',
+    port: 1025,
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -41,7 +50,6 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
