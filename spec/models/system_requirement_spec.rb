3e6bae998305d1bf796c5a09f24318ab0fa5113c
CaioFML <caio.felipemotta@hotmail.com>
2020-11-09 20:33:11 -0300

    fix

--- spec/models/system_requirement_spec.rb
+++ spec/models/system_requirement_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe SystemRequirement do
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:operational_system) }
   it { is_expected.to validate_presence_of(:storage) }
   it { is_expected.to validate_presence_of(:processor) }
