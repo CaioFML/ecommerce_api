aa34fea796439949d946a434b1d7f145b38f67fe
CaioFML <caio.felipemotta@hotmail.com>
2020-11-09 21:14:22 -0300

    Model Polimórfico de Produtos

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,4 +1,6 @@
 RSpec.describe Category do
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  end
 end
