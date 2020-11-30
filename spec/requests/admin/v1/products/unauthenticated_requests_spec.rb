fa5d22fb2ac457e77259b81ec9c0aa3dfef0fa1c
CaioFML <caio.felipemotta@hotmail.com>
2020-11-30 19:56:22 -0300

    Explicando os Testes de Request de Produtos

---
+++ spec/requests/admin/v1/products/unauthenticated_requests_spec.rb
@@ -0,0 +1,47 @@
+RSpec.describe Admin::V1::ProductsController do
+  context "without authentication" do
+    context "GET /products" do
+      let(:url) { "/admin/v1/products" }
+      let!(:products) { create_list(:product, 5) }
+
+      before(:each) { get url }
+
+      include_examples "unauthenticated access"
+    end
+
+    context "POST /products" do
+      let(:url) { "/admin/v1/products" }
+
+      before(:each) { post url }
+
+      include_examples "unauthenticated access"
+    end
+
+    context "GET /products/:id" do
+      let(:product) { create(:product) }
+      let(:url) { "/admin/v1/products/#{product.id}" }
+
+      before(:each) { get url }
+
+      include_examples "unauthenticated access"
+    end
+
+    context "PATCH /products/:id" do
+      let(:product) { create(:product) }
+      let(:url) { "/admin/v1/products/#{product.id}" }
+
+      before(:each) { patch url }
+
+      include_examples "unauthenticated access"
+    end
+
+    context "DELETE /products/:id" do
+      let!(:product) { create(:product) }
+      let(:url) { "/admin/v1/products/#{product.id}" }
+
+      before(:each) { delete url }
+
+      include_examples "unauthenticated access"
+    end
+  end
+end
